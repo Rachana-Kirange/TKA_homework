292ef132801e14144f1afb78c34dc7c73dd607f5
Rachana-Kirange <rmkirange29@gmail.com>
2025-06-30 21:10:32 +0530

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -30,9 +30,35 @@ class F(C,D):
         print("Floor Division = ",a,"//",b,"=",a//b)
 e = E()
 f = F()
-# e.add()
-# e.sub()
+e.add()
+e.sub()
 f.add()
 f.div()
 f.floor()
 f.mult()
+
+
+'''
+OUTPUT :-
+Enter the number :- 12
+Enter the number :- 3
+Addition =  12 + 3 = 15
+Enter the number :- 123
+Enter the number :- 34
+Substraction =  123 - 34 = 89
+Enter the number :- 34
+Enter the number :- 4
+Addition =  34 % 4 = 2
+Enter the number :- 56
+Enter the number :- 3
+Addition =  56 + 3 = 59
+Enter the number :- 456
+Enter the number :- 4 
+Division =  456 / 4 = 114.0
+Enter the number :-42 
+Enter the number :- 3
+Floor Division =  42 // 3 = 14
+Enter the number :-40 
+Enter the number :- 4
+Multiplication =  40 * 4 = 160
+'''
